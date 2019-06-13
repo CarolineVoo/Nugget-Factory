5419d86910ba8b4f1a9fde74cee686e7dc2954e3
CarolineVoo <33224577+CarolineVoo@users.noreply.github.com>
2019-06-13 22:19:13 +0200

    test

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function selectNug(){
     var img = document.getElementById("showNugget");
     img.src = this.value;
-    return false;
 }
 document.getElementById("nuggetType").onchange = selectNug;
 
